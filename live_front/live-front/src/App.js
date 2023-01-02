0b1937f4a9e70dd57a0abd77d842e559aad36b59
Walter Thomas <walterdthomas@me.com>
2023-01-02 16:18:55 -0500

    new start

---
+++ live_front/live-front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
