7f2fee091c6a516b1014b25f2e22dc3a1dcdab3f
Walter Thomas <walterdthomas@me.com>
2023-01-06 11:54:05 -0500

    Walter Thomas

--- live_front/live-front/src/App.js
+++ live_front/live-front/src/App.js
@@ -1,13 +1,11 @@
 import './App.css'
-import { useState } from 'react'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
 
 function App() {
 
-  const [currentVenue, setCurrentVenue] = useState({})
-  const [currentBand, setCurrentBand] = useState({})
+  
 
   return (    
       <div className="App">
