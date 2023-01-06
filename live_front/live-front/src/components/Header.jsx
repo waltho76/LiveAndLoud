3937cfec82816ab33c03700b2073923dd6e641a0
Walter Thomas <walterdthomas@me.com>
2023-01-05 22:14:06 -0500

    working

--- live_front/live-front/src/components/Header.jsx
+++ live_front/live-front/src/components/Header.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-
+import Livelogo from './Livelogo.png'
 
 export default function Header () {
     return (
 
+        
         <div className='header'>
-          <h1>LIVE-N-LOUD</h1>            
+            <div className='logo-container'>
+            <img src={Livelogo} alt='live n loud'/>
+            </div> 
+                      {/* <h1>LIVE-N-LOUD</h1> */}
             <div className='nav'>
                 <Link className='nav-link' to='/'>Home</Link>
                 <Link className='nav-link' to='/city'>CITIES</Link>
                 <Link className='nav-link' to='/venue'>VENUES</Link>
                 <Link className='nav-link' to='/band'>BANDS</Link>
-            </div>
+            </div> 
             
+                      
         </div>
     )
 }
